e9fe98aef6426252b5e5a36c37021b6b9fb82f3c
Benny Smith <asmit101@node1322.oscar.ccv.brown.edu>
2020-08-25 17:38:46 -0400

    finishing adding things in from Oscar

---
+++ looping_through_lastone.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -n 12
+#SBATCH --mem=64G
+#SBATCH -t 48:00:00
+module load nco/4.9.3
+cd /users/dullman1/data/dullman1/EPSCOR/2018_mod8_run1_NAM
+for file in ocean_his_*.nc ; do
+        cd /users/dullman1/data/dullman1/EPSCOR/2018_mod8_run1_NAM
+        cp $file /users/asmit101/data/stuff/2018_mod8_run1_NAM
+        cd /users/asmit101/data/stuff/2018_mod8_run1_NAM
+        ncks -d s_rho,-1 -d eta_rho,0,600 -v chlorophyll1,chlorophyll2 $file surfchl_$file
+        rm $file
+        cd /users/dullman1/data/dullman1/EPSCOR/2018_mod8_run1_NAM
+        echo "Processing $file"
+done
